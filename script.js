bfbd62d2c7f403e4edde960aa5252a57151441af
Paul J. Shaw <pavlvsxavier@gmail.com>
2020-12-28 10:11:49 +0000

    added hold button functionality and game state

--- script.js
+++ script.js
@@ -19,32 +19,63 @@ score1El.textContent = 0
 const scores = [0, 0]
 let currentScore = 0
 let activePlayer = 0
+let gameOn = true
+
+const switchPlayers = function () {
+    currentScore = 0
+    activePlayer = activePlayer === 0 ? 1 : 0
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
+}
 
 diceEl.classList.add('hidden')
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', ev => {
-    // 1. Generate random die roll between 1 and 6
-    const diceRoll = Math.trunc(Math.random() * 6) + 1
+    if (gameOn) {
+        // 1. Generate random die roll between 1 and 6
+        const diceRoll = Math.trunc(Math.random() * 6) + 1
 
-    // 2. Display die
-    diceEl.src = `images/dice-${diceRoll}.png`
-    diceEl.classList.remove('hidden')
+        // 2. Display die
+        diceEl.src = `images/dice-${diceRoll}.png`
+        diceEl.classList.remove('hidden')
 
-    // 3. Check for a 1. If true switch player
-    if (diceRoll === 1) {
-        currentScore = 0
-        document.getElementById(
-            `current--${activePlayer}`
-        ).textContent = currentScore
-        activePlayer = activePlayer === 0 ? 1 : 0
-        player0El.classList.toggle('player-active')
-        player1El.classList.toggle('player-active')
-    } else {
-        // Add diceRoll to current score
-        currentScore += diceRoll
-        document.getElementById(
-            `current--${activePlayer}`
-        ).textContent = currentScore
+        // 3. Check for a 1. If true switch player
+        if (diceRoll === 1) {
+            document.getElementById(`current--${activePlayer}`).textContent = 0
+            switchPlayers()
+        } else {
+            // Add diceRoll to current score
+            currentScore += diceRoll
+            document.getElementById(
+                `current--${activePlayer}`
+            ).textContent = currentScore
+        }
+    }
+})
+
+// Holding functionality
+btnHold.addEventListener('click', ev => {
+    if (gameOn) {
+        // 1. Add current score to active player's total score and display it
+        scores[activePlayer] += currentScore
+        document.getElementById(`score--${activePlayer}`).textContent =
+            scores[activePlayer]
+        // 2. Reset current score
+        document.getElementById(`current--${activePlayer}`).textContent = 0
+        // 3. If total score below 100 Switch players
+        if (scores[activePlayer] < 20) {
+            switchPlayers()
+        } else {
+            // finish the game
+            gameOn = false
+            document
+                .querySelector(`.player--${activePlayer}`)
+                .classList.add('player--winner')
+            document
+                .querySelector(`.player--${activePlayer}`)
+                .classList.remove('player--active')
+            diceEl.classList.add('hidden')
+        }
     }
 })
